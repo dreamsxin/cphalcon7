cdca39044e26cf8abf51183b2733cb0053bef60a
朱宗鑫 <dreamsxin@qq.com>
2016-08-31 14:33:00 +0800

    Updated class Phalcon\Async

--- ext/async.c
+++ ext/async.c
@@ -227,8 +227,14 @@ PHP_METHOD(Phalcon_Async, recvAll){
 
 	while(i--) {
 		zval type = {}, message = {}, result = {};
+		ZVAL_NULL(&type);
+		ZVAL_NULL(&message);
 
+		ZVAL_MAKE_REF(&type);
+		ZVAL_MAKE_REF(&message);
 		PHALCON_CALL_FUNCTIONW(&result, "msg_receive", &seg, &pid, &type, &size, &message, &PHALCON_GLOBAL(z_true), &flag);
+		ZVAL_UNREF(&message);
+		ZVAL_UNREF(&type);
 		if (zend_is_true(&result)) {
 			phalcon_array_update_zval(return_value, &type, &message, PH_COPY);
 		} else {

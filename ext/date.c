6b84f44228142ca57daaebea623de954a09c48eb
朱宗鑫 <dreamsxin@qq.com>
2016-10-20 11:53:08 +0800

    Updated class Phalcon\Date

--- ext/date.c
+++ ext/date.c
@@ -337,7 +337,6 @@ PHP_METHOD(Phalcon_Date, hours){
 		}
 	}
 
-
 	if (!start || Z_TYPE_P(start) == IS_NULL) {
 		if (is_long && zend_is_true(is_long)) {
 			s = 0;
@@ -601,8 +600,15 @@ PHP_METHOD(Phalcon_Date, span){
 	phalcon_fetch_params(0, 1, 2, &remote, &local, &output);
 
 	if (!output) {
-		ZVAL_STRING(&lowercased_output, "years,months,weeks,days,hours,minutes,seconds");
-	} else {
+		array_init(&output_arr);
+		phalcon_array_append_string(&output_arr, SL("years"), 0);
+		phalcon_array_append_string(&output_arr, SL("months"), 0);
+		phalcon_array_append_string(&output_arr, SL("weeks"), 0);
+		phalcon_array_append_string(&output_arr, SL("days"), 0);
+		phalcon_array_append_string(&output_arr, SL("hours"), 0);
+		phalcon_array_append_string(&output_arr, SL("minutes"), 0);
+		phalcon_array_append_string(&output_arr, SL("seconds"), 0);
+	} else if (Z_TYPE_P(output) != IS_ARRAY) {
 		ZVAL_STR(&tmp, phalcon_trim(output, NULL, PHALCON_TRIM_BOTH));
 
 		phalcon_fast_strtolower(&lowercased_output, &tmp);
@@ -610,11 +616,12 @@ PHP_METHOD(Phalcon_Date, span){
 		if (PHALCON_IS_EMPTY(&lowercased_output)) {
 			RETURN_FALSE;
 		}
+
+		ZVAL_STRING(&pattern, "/[^a-z]+/");
+		PHALCON_CALL_FUNCTIONW(&output_arr, "preg_split", &pattern, &lowercased_output);
+	} else {
+		PHALCON_CPY_WRT_CTOR(&output_arr, output);
 	}
-
-	ZVAL_STRING(&pattern, "/[^a-z]+/");
-
-	PHALCON_CALL_FUNCTIONW(&output_arr, "preg_split", &pattern, &lowercased_output);
 
 	ZVAL_LONG(&tmp1, 0);
 
@@ -1071,14 +1078,14 @@ PHP_METHOD(Phalcon_Date, formatted_time){
 }
 
 /**
- * Returns a date/time string with the specified timestamp format
+ * Checks if a value is a valid date
  *
  *     $ret = Phalcon\Date::valid('2012-01-22');
  *     $ret = Phalcon\Date::valid('2012-01-22 11:00:00', 'Y-m-d H:i:s');
  *
  * @param string $date_str
  * @param string $date_format
- * @return string
+ * @return boolean
  */
 PHP_METHOD(Phalcon_Date, valid){
 

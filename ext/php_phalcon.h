b9247aecc548d762851e0a11836404966ea180f0
朱宗鑫 <dreamsxin@qq.com>
2016-10-21 11:25:58 +0800

    Upgrade version to 1.1.1

--- ext/php_phalcon.h
+++ ext/php_phalcon.h
@@ -29,7 +29,7 @@
 #include <TSRM/TSRM.h>
 #endif
 
-#define PHP_PHALCON_VERSION "Dao7-1.1.0"
+#define PHP_PHALCON_VERSION "Dao7-1.1.1"
 #define PHP_PHALCON_EXTNAME "phalcon7"
 
 /** DEBUG options */

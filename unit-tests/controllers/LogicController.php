badee4ee7d84bf00685ec7f8a876a4a8e5836443
朱宗鑫 <dreamsxin@qq.com>
2016-11-04 17:22:00 +0800

    Updated about logic binding

--- unit-tests/controllers/LogicController.php
+++ unit-tests/controllers/LogicController.php
@@ -3,8 +3,10 @@
 class LogicController extends Phalcon\Mvc\Controller
 {
 
-	public function indexAction(\MyLogic $logic)
+	public function indexAction(\MyLogic $logic, $param2, $param1)
 	{
+		$logic->param1 = $param1;
+		$logic->param2 = $param2;
 		return $logic;
 	}
 

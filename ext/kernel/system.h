d2326022a3f85f3f2e49c5dfe763eb41e53f3b36
dreamszhu <dreamsxin@qq.com>
2016-12-08 19:13:10 +0800

    Updated kernel/system.c

--- ext/kernel/system.h
+++ ext/kernel/system.h
@@ -28,4 +28,86 @@
 long int phalcon_get_system_uptime();
 long int phalcon_get_proc_starttime(unsigned int pid);
 
+/* structs */
+typedef struct _phalcon_process {
+	zend_bool has_commandline;
+	unsigned int pid;
+	int iopriority;
+
+	/* cmdline */
+	zval args;
+
+	/* stat */
+    char name[256];
+	char state;
+	unsigned int ppid;
+	unsigned int pgrp;
+	unsigned int session;
+	unsigned int tty_nr;
+	unsigned int tpgid;
+	unsigned int rchar;
+	unsigned int wchar;
+	unsigned int flags;
+	unsigned long minflt;
+	unsigned long cminflt;
+	unsigned long majflt;
+	unsigned long cmajflt;
+	unsigned long utime;
+	unsigned long stime;
+	long int cutime;
+	long int cstime;
+	long int priority;
+	long int nice;
+	long int numthreads;
+	unsigned long long starttime;
+	unsigned long vsize;
+	long int rss;
+	unsigned long rsslim;
+	unsigned long startcode;
+	unsigned long endcode;
+	unsigned long startstack;
+	unsigned long kstkesp;
+	unsigned long kstkeip;
+	unsigned long wchan;
+	int exit_signal;
+	int processor;
+	unsigned int rt_priority;
+	unsigned int policy;
+	unsigned long long delayacct_blkio_ticks;
+	unsigned long guest_time;
+	long int cguest_time;
+
+	/* status */
+	int ruid;
+	int euid;
+	int ssuid;
+	int fsuid;
+	int rgid;
+	int egid;
+	int ssgid;
+	int fsgid;
+
+	/* statm */
+	unsigned int size;
+	unsigned int resident;
+	unsigned int share;
+	unsigned int text;
+	unsigned int data;
+
+	/* io */
+	unsigned int syscr;
+	unsigned int syscw;
+	unsigned int read_bytes;
+	unsigned int write_bytes;
+	unsigned int cancelled_write_bytes;
+} phalcon_process;
+
+int phalcon_get_proc_info(phalcon_process *proc, unsigned int pid);
+int phalcon_proc_parse_stat(phalcon_process *proc, unsigned int pid);
+int phalcon_proc_parse_statm(phalcon_process *proc, unsigned int pid);
+int phalcon_proc_parse_status(phalcon_process *proc, unsigned int pid);
+int phalcon_proc_parse_io(phalcon_process* proc, unsigned int pid);
+int phalcon_proc_parse_cmdline(phalcon_process *proc, unsigned int pid);
+int phalcon_proc_get_iopriority(unsigned int pid);
+
 #endif /* PHALCON_KERNEL_SYSTEM_H */

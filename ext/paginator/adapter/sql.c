bb879be06b6e74639d05a9b3c73e1b57ba5a49cf
dreamszhu <dreamsxin@qq.com>
2017-04-08 21:41:32 +0800

    Updated class Phalcon\Tag\Select

--- ext/paginator/adapter/sql.c
+++ ext/paginator/adapter/sql.c
@@ -251,7 +251,7 @@ PHP_METHOD(Phalcon_Paginator_Adapter_Sql, setDb){
 	if (Z_TYPE_P(dbname) != IS_OBJECT) {
 		PHALCON_CALL_METHOD(&db, getThis(), "getresolveservice", dbname);
 	} else {
-		PHALCON_CPY_WRT_CTOR(&db, dbname);
+		ZVAL_COPY_VALUE(&db, dbname);
 	}
 	PHALCON_VERIFY_INTERFACE_EX(&db, phalcon_db_adapterinterface_ce, phalcon_paginator_exception_ce);
 

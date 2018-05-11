44d1a4135118e38b82b744b9efc4a91600e162b0
Dreamszhu <dreamsxin@qq.com>
2018-05-11 20:22:41 +0800

    Add method Phalcon\Date::add

--- unit-tests/DateTest.php
+++ unit-tests/DateTest.php
@@ -32,6 +32,12 @@ class DateTest extends PHPUnit\Framework\TestCase
 		$this->assertTrue(\Phalcon\Date::valid("2012-12-12"));
 		$this->assertTrue(\Phalcon\Date::valid("2012-12-12 11:11:11", 'Y-m-d H:i:s'));
 		$this->assertFalse(\Phalcon\Date::valid("2012-12-12 11:11:11"));
+
+		$date = \Phalcon\Date::add('2000-01-01', '1 days');
+		$this->assertEquals($date, '2000-01-02');
+
+		$date = \Phalcon\Date::add('2000-01-01', '-1 days');
+		$this->assertEquals($date, '1999-12-31');
 	}
 
 }

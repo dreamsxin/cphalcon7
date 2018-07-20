304bb8e4e8fca60e11889902b6b426b11531e947
Dreamszhu <dreamsxin@qq.com>
2018-07-20 16:27:43 +0800

    Fix bug in method Phalcon\Backend\Redis::delete

--- ext/cache/backend/redis.c
+++ ext/cache/backend/redis.c
@@ -393,10 +393,10 @@ PHP_METHOD(Phalcon_Cache_Backend_Redis, delete){
 	if (phalcon_array_isset_fetch_str(&special_key, &options, SL("statsKey"), PH_READONLY) && PHALCON_IS_NOT_EMPTY_STRING(&special_key)) {
 		PHALCON_CALL_METHOD(NULL, &redis, "srem", &special_key, &prefixed_key);
 	}
-	zval_ptr_dtor(&prefixed_key);
 
 	/* Delete the key from redisd */
 	PHALCON_CALL_METHOD(&ret, &redis, "delete", &prefixed_key);
+	zval_ptr_dtor(&prefixed_key);
 	zval_ptr_dtor(&redis);
 	if (zend_is_true(&ret)) {
 		RETURN_TRUE;

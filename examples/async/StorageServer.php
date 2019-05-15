1c34022b9a4cdaa0d9c2c9df8b675e0293d446b6
dreamsxin <dreamsxin@126.com>
2019-05-15 16:11:44 +0800

    Add example/StorageServer.php

---
+++ examples/async/StorageServer.php
@@ -0,0 +1,88 @@
+<?php
+
+
+$lmdb = new Phalcon\Storage\Libmdbx('./testdb');
+
+$server = Phalcon\Async\Network\TcpServer::listen('localhost', 8888);
+
+function generateChunk($sendchunk) {
+	return \sprintf("HTTP/1.1 200 OK\r\nServer: webserver\r\nContent-Type: text/html\r\nTransfer-Encoding: chunked\r\nConnection: close\r\n\r\n%x\r\n%s\r\n0\r\n\r\n", \strlen($sendchunk), $sendchunk);
+}
+
+try {
+	while (true) {
+		$socket = $server->accept();
+		if ($socket === false) {
+			continue;
+		}
+		Phalcon\Async\Task::async(function () use ($socket, $lmdb) {
+			echo 'New client'.PHP_EOL;
+			try {
+
+				$buffer = '';
+				$content_length = 0;
+				$content = '';
+					
+				$parser = new Phalcon\Http\Parser();
+				while (!empty($chunk = $socket->read())) {
+					$buffer .= $chunk;
+					
+					if (($result = $parser->execute($chunk))) {
+						if (isset($result['BODY'])) {
+							$content = $result['BODY'];
+						}
+						if (isset($result['HEADERS']) && isset($result['HEADERS']['Content-Length'])) {
+							$content_length = $result['HEADERS']['Content-Length'];
+							break;
+						}
+					}
+					if (!$content_length) {
+						if (substr_compare($chunk, "\r\n", -2, 2) === 0) {
+							break;
+						}
+					} elseif (strlen($content) >= $content_length){
+						break;
+					}
+				}
+				
+				echo 'Content '.$content_length.', '.$content.PHP_EOL;
+
+				$parts = explode(" ", $content, 3);
+				if (count($parts) < 2) {
+					$sendchunk = generateChunk('error');
+					$socket->write($sendchunk);
+					return;
+				}
+				$op = $parts[0];
+				$key = $parts[1];
+				if ($op == 'GET') {
+					$lmdb->begin();
+					$sendchunk = 'value '.$lmdb->get($key);
+					$lmdb->commit();
+				} elseif ($op == 'SET' || $op == 'PUT') {
+					if (!isset($parts[2])) {
+						$sendchunk = generateChunk('error');
+						$socket->write($sendchunk);
+						return;
+					}
+					$sendchunk = 'ok';
+					$value = $parts[2];
+					$lmdb->begin();
+					$lmdb->put($key, $value);
+					$lmdb->commit();
+				} else {
+					$sendchunk = 'error op is not valid';
+				}
+
+				$sendchunk = generateChunk($sendchunk);
+				$socket->write($sendchunk);
+			} catch (\Throwable $e) {
+				echo $e, "\n\n";
+			} finally {
+				$socket->close();
+			}
+		});
+	}
+} finally {
+	$server->close();
+}

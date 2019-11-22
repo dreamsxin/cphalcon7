ff7e3f8552a2dae9dbb554fa456ebb3373819e13
Dreamszhu <dreamsxin@qq.com>
2019-11-22 21:49:27 +0800

    Add method Phalcon\Image::splitHexColor

--- ext/image.c
+++ ext/image.c
@@ -26,6 +26,7 @@
 #include "kernel/memory.h"
 #include "kernel/fcall.h"
 #include "kernel/object.h"
+#include "kernel/array.h"
 
 /**
  * Phalcon\Image
@@ -41,6 +42,7 @@
 zend_class_entry *phalcon_image_ce;
 
 PHP_METHOD(Phalcon_Image, factory);
+PHP_METHOD(Phalcon_Image, splitHexColor);
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_image_factory, 0, 0, 1)
 	ZEND_ARG_INFO(0, file)
@@ -48,8 +50,13 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_image_factory, 0, 0, 1)
 	ZEND_ARG_INFO(0, height)
 ZEND_END_ARG_INFO()
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_image_splithexcolor, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, color, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
 static const zend_function_entry phalcon_image_method_entry[] = {
 	PHP_ME(Phalcon_Image, factory, arginfo_phalcon_image_factory, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
+	PHP_ME(Phalcon_Image, splitHexColor, arginfo_phalcon_image_splithexcolor, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
 	PHP_FE_END
 };
 
@@ -107,4 +114,88 @@ PHP_METHOD(Phalcon_Image, factory)
 		object_init_ex(return_value, phalcon_image_adapter_gd_ce);
 		PHALCON_CALL_METHOD(NULL, return_value, "__construct", file, width, height);
 	}
+}
+
+
+void str_repeat(zval *return_value, zend_string *input_str, zend_long mult)
+{
+	zend_string	*result;		/* Resulting string */
+	size_t result_len;		/* Length of the resulting string */
+
+	if (mult < 0) {
+		return;
+	}
+
+	/* Don't waste our time if it's empty */
+	/* ... or if the multiplier is zero */
+	if (ZSTR_LEN(input_str) == 0 || mult == 0)
+		RETURN_EMPTY_STRING();
+
+	/* Initialize the result string */
+	result = zend_string_safe_alloc(ZSTR_LEN(input_str), mult, 0, 0);
+	result_len = ZSTR_LEN(input_str) * mult;
+
+	/* Heavy optimization for situations where input string is 1 byte long */
+	if (ZSTR_LEN(input_str) == 1) {
+		memset(ZSTR_VAL(result), *ZSTR_VAL(input_str), mult);
+	} else {
+		char *s, *e, *ee;
+		ptrdiff_t l=0;
+		memcpy(ZSTR_VAL(result), ZSTR_VAL(input_str), ZSTR_LEN(input_str));
+		s = ZSTR_VAL(result);
+		e = ZSTR_VAL(result) + ZSTR_LEN(input_str);
+		ee = ZSTR_VAL(result) + result_len;
+
+		while (e<ee) {
+			l = (e-s) < (ee-e) ? (e-s) : (ee-e);
+			memmove(e, s, l);
+			e += l;
+		}
+	}
+
+	ZSTR_VAL(result)[result_len] = '\0';
+
+	RETURN_NEW_STR(result);
+}
+
+PHP_METHOD(Phalcon_Image, splitHexColor)
+{
+	zval *str, newcolor = {};
+	unsigned int color[4];
+	const char *value;
+	int len, count;
+
+	phalcon_fetch_params(0, 1, 2, &str);
+
+	len = Z_STRLEN_P(str);
+
+	if(len < 6) {
+		str_repeat(&newcolor, Z_STR_P(str), 6);
+	} else {
+		ZVAL_COPY(&newcolor, str);
+	}
+	value = Z_STRVAL(newcolor);
+	if(len == 8) {
+		count = sscanf(value, "%02x%02x%02x%02x%n", &color[0], &color[1], &color[2], &color[3], &len);
+		if(count < 4 || len != 8) {
+			RETVAL_FALSE;
+		} else {
+			array_init(return_value);
+			phalcon_array_append_long(return_value, color[0], 0);
+			phalcon_array_append_long(return_value, color[1], 0);
+			phalcon_array_append_long(return_value, color[2], 0);
+			phalcon_array_append_long(return_value, color[3], 0);
+		}
+	} else {
+		count = sscanf(value, "%02x%02x%02x%n", &color[0], &color[1], &color[2], &len);
+		if(count < 3 || len != 6) {
+			RETVAL_FALSE;
+		} else {
+			array_init(return_value);
+			phalcon_array_append_long(return_value, color[0], 0);
+			phalcon_array_append_long(return_value, color[1], 0);
+			phalcon_array_append_long(return_value, color[2], 0);
+		}
+	}
+	zval_ptr_dtor(&newcolor);
 }

fcf950360d26f5ec92bf2172ddb01285a9260992
dreamsxin <dreamsxin@126.com>
2019-12-04 15:03:38 +0800

    Add example async/proc.php

---
+++ examples/async/proc.php
@@ -0,0 +1,24 @@
+<?php
+
+error_reporting(-1);
+
+$process = Phalcon\Async\Process\ProcessBuilder::fork(__DIR__ . '/process/worker.php')->start();
+$ipc = $process->getIpc();
+
+try {
+    $tcp = Phalcon\Async\Network\TcpSocket::connect('httpbin.org', 80);
+    
+    Phalcon\Async\Task::async([
+        $tcp,
+        'write'
+    ], "GET /json HTTP/1.0\r\nHost: httpbin.org\r\nConnection: close\r\n\r\n");
+    
+    var_dump('SEND HANDLE');
+    $tcp->export($ipc);
+    $tcp->close();    
+} finally {
+    $ipc->close();
+}
+
+var_dump('AWAIT DATA...');
+printf("\nEXIT CODE: %u\n", $process->join());

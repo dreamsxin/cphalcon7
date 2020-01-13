94b55563cdc3fad0855c31f9e891213e55613633
dreamsxin <dreamsxin@126.com>
2020-01-13 11:53:16 +0800

    Updated

--- ext/arr/int.c
+++ ext/arr/int.c
@@ -47,7 +47,8 @@ PHP_METHOD(Phalcon_Arr_Int, set);
 PHP_METHOD(Phalcon_Arr_Int, get);
 PHP_METHOD(Phalcon_Arr_Int, add);
 PHP_METHOD(Phalcon_Arr_Int, insert);
-PHP_METHOD(Phalcon_Arr_Int, sort);
+PHP_METHOD(Phalcon_Arr_Int, size);
+PHP_METHOD(Phalcon_Arr_Int, capacity);
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_arr_int___construct, 0, 0, 0)
 	ZEND_ARG_TYPE_INFO(0, size, IS_LONG, 1)
@@ -66,11 +67,19 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_arr_int_add, 0, 0, 1)
 	ZEND_ARG_TYPE_INFO(0, value, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_arr_int_insert, 0, 0, 2)
+	ZEND_ARG_TYPE_INFO(0, index, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, value, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
 static const zend_function_entry phalcon_arr_int_method_entry[] = {
 	PHP_ME(Phalcon_Arr_Int, __construct, arginfo_phalcon_arr_int___construct, ZEND_ACC_PUBLIC|ZEND_ACC_CTOR)
 	PHP_ME(Phalcon_Arr_Int, set, arginfo_phalcon_arr_int_set, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Arr_Int, get, arginfo_phalcon_arr_int_get, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Arr_Int, add, arginfo_phalcon_arr_int_add, ZEND_ACC_PUBLIC)
+	PHP_ME(Phalcon_Arr_Int, insert, arginfo_phalcon_arr_int_insert, ZEND_ACC_PUBLIC)
+	PHP_ME(Phalcon_Arr_Int, size, NULL, ZEND_ACC_PUBLIC)
+	PHP_ME(Phalcon_Arr_Int, capacity, NULL, ZEND_ACC_PUBLIC)
 	PHP_FE_END
 };
 
@@ -102,6 +111,8 @@ PHALCON_INIT_CLASS(Phalcon_Arr_Int){
 
 	PHALCON_REGISTER_CLASS_CREATE_OBJECT(Phalcon\\Arr, Int, arr_int, phalcon_arr_int_method_entry, 0);
 
+	//zend_class_implements(phalcon_arr_int_ce, 5, zend_ce_iterator, spl_ce_SeekableIterator, spl_ce_Countable, zend_ce_arrayaccess, zend_ce_serializable);
+
 	return SUCCESS;
 }
 
@@ -120,14 +131,14 @@ PHP_METHOD(Phalcon_Arr_Int, __construct){
 	if (size && Z_LVAL_P(size) > 0) {
 		intern->len = Z_LVAL_P(size);
 	} else {
-		intern->len = 1;
+		intern->len = 0;
 	}
 	intern->total = intern->len > 100 ? intern->len : 100;
-	intern->value = ecalloc(intern->len, sizeof(zend_long));
+	intern->value = ecalloc(intern->total, sizeof(zend_long));
 }
 
 /**
- *  Sets value
+ * Sets value
  *
  * @return boolean
  */
@@ -148,7 +159,7 @@ PHP_METHOD(Phalcon_Arr_Int, set){
 }
 
 /**
- *  Gets value
+ * Gets value
  *
  * @return int
  */
@@ -168,7 +179,7 @@ PHP_METHOD(Phalcon_Arr_Int, get){
 }
 
 /**
- *  Add value
+ * Add value
  *
  * @return boolean
  */
@@ -182,10 +193,71 @@ PHP_METHOD(Phalcon_Arr_Int, add){
 	intern = phalcon_arr_int_object_from_obj(Z_OBJ_P(getThis()));
 
 	if (intern->len >= intern->total) {
-		erealloc(intern->value, (intern->total+100)*sizeof(zend_long));
-		intern->total += 100;
+		int num = intern->len * 5;
+		intern->value = erealloc(intern->value, (intern->total+num)*sizeof(zend_long));
+		intern->total += num;
 	}
+
 	intern->value[intern->len] = Z_LVAL_P(value);
 	intern->len += 1;
 	RETURN_TRUE;
+}
+
+/**
+ * Insert value
+ *
+ * @return boolean
+ */
+PHP_METHOD(Phalcon_Arr_Int, insert){
+
+	zval *index, *value;
+	phalcon_arr_int_object *intern;
+	int idx;
+
+	phalcon_fetch_params(0, 2, 0, &index, &value);
+
+	intern = phalcon_arr_int_object_from_obj(Z_OBJ_P(getThis()));
+
+	idx = Z_LVAL_P(index);
+
+	if (idx < 0 || idx >= intern->len) {
+		RETURN_FALSE;
+	}
+	if (intern->len >= intern->total) {
+		int num = intern->len * 5;
+		intern->value = erealloc(intern->value, (intern->total+num)*sizeof(zend_long));
+		intern->total += num;
+	}
+	memcpy(intern->value+idx+1, intern->value+idx, (intern->len - idx)*sizeof(zend_long));
+	intern->len += 1;
+	intern->value[idx] = Z_LVAL_P(value);
+	RETURN_TRUE;
+}
+
+/**
+ * Gets the size
+ *
+ * @return int
+ */
+PHP_METHOD(Phalcon_Arr_Int, size){
+
+	phalcon_arr_int_object *intern;
+
+	intern = phalcon_arr_int_object_from_obj(Z_OBJ_P(getThis()));
+
+	RETURN_LONG(intern->len);
+}
+
+/**
+ * Gets the capacity
+ *
+ * @return int
+ */
+PHP_METHOD(Phalcon_Arr_Int, capacity){
+
+	phalcon_arr_int_object *intern;
+
+	intern = phalcon_arr_int_object_from_obj(Z_OBJ_P(getThis()));
+
+	RETURN_LONG(intern->total);
 }

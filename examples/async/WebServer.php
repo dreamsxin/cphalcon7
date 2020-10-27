3644c90e058b1855625cd390090534f0f59b81a2
dreamsxin <dreamsxin@126.com>
2020-10-27 10:17:04 +0800

    Updated example webserver

--- examples/async/WebServer.php
+++ examples/async/WebServer.php
@@ -4,25 +4,14 @@
  * curl http://localhost:8080/index/index
  * curl http://localhost:8080/index/view
  */
-$loader = new \Phalcon\Loader();
 
-$loader->registerDirs(
-		array(
-			__DIR__.'/mvc/controller',
-		)
-)->register();
-
-$logfile = __DIR__ . '/mvc/debug.log';
-$logger = new \Phalcon\Logger\Adapter\File($logfile);
-
-function debug(string $data, $lineno = 'NULL') {
-
-	$message = ['data' => $data, 'line' => $lineno];
-	print_r($data);
+$worknum = 4;
+for ($i=0; $i < $worknum; $i++) {
+	$worker[$i] = Phalcon\Async\Process\ProcessBuilder::fork(__DIR__ . '/webserver/worker.php')->start();
 }
-
 $server = Phalcon\Async\Network\TcpServer::listen('localhost', 8080);
 
+$num = 0;
 try {
 	var_dump($server->getAddress(), $server->getPort());
 
@@ -31,67 +20,9 @@ try {
 		if ($socket === false) {
 			continue;
 		}
-		Phalcon\Async\Task::async(function () use ($socket) {
-			//\Phalcon\Debug::enable();
-			try {
-				$uri = $chunk = '';
-				$ret = NULL;
-				$parser = new \Phalcon\Http\Parser();
-				while($buffer = $socket->read()) {
-					$ret = $parser->execute($buffer);
-					if (!$ret) {
-						throw new \Exception('HTTP parse failed');
-					}
-					if ($parser->status() == \Phalcon\Http\Parser::STATUS_END) {
-						$uri = \Phalcon\Arr::get($ret, 'QUERY_STRING');
-						$body = \Phalcon\Arr::get($ret, 'BODY');
-						break;
-					}
-				}
-
-				if (empty($ret)) {
-					$socket->close();
-					return;
-				}
-
-				$di = new \Phalcon\DI;
-				$di->set('dispatcher', function () {
-					$dispatcher = new \Phalcon\Mvc\Dispatcher();
-					return $dispatcher;
-				}, TRUE);
-				$di->set('request', function () {
-					$request = new \Phalcon\Http\Request();
-					return $request;
-				}, TRUE);
-				$di->set('response', function () {
-					$response = new \Phalcon\Http\Response();
-					return $response;
-				}, TRUE);
-				$di->set('router', function () {
-					$router = new \Phalcon\Mvc\Router();
-					return $router;
-				}, TRUE);
-				$di->set('view', function () {
-					$view = new \Phalcon\Mvc\View();
-					$view->setBasePath(__DIR__.DIRECTORY_SEPARATOR.'mvc/views');
-					return $view;
-				}, TRUE);
-
-				$application = new \Phalcon\Mvc\Application;
-				$application->useImplicitView(false);
-				$sendchunk = $application->handle($uri)->getContent();
-
-				//debug($sendchunk, __LINE__);
-				$sendchunk = \sprintf("HTTP/1.1 200 OK\r\nServer: webserver\r\nContent-Type: text/html\r\nTransfer-Encoding: chunked\r\nConnection: close\r\n\r\n%x\r\n%s\r\n0\r\n\r\n", \strlen($sendchunk), $sendchunk);
-				$socket->write($sendchunk);
-			} catch (\Throwable $e) {
-				debug($e->getMessage(), __LINE__);
-			} finally {
-				//\Phalcon\Debug::disable();
-				$socket->close();
-				//debug('CLIENT DISCONNECTED', __LINE__);
-			}
-		});
+		$num++;
+		$socket->export($worker[$num%$worknum]->getIpc());
+		$socket->close();
 	}
 } finally {
 	$server->close();
---
+++ examples/async/webserver/worker.php
@@ -0,0 +1,104 @@
+<?php
+
+var_dump(Phalcon\Async\Process\Process::isWorker());
+var_dump($ipc = Phalcon\Async\Process\Process::connect());
+
+$loader = new \Phalcon\Loader();
+
+$loader->registerDirs(
+		array(
+			__DIR__.'/mvc/controller',
+		)
+)->register();
+
+function debug(string $data, $lineno = 'NULL') {
+	$message = ['data' => $data, 'line' => $lineno];
+	print_r($data);
+}
+
+while ($socket = Phalcon\Async\Network\TcpSocket::import($ipc)) {
+
+	Phalcon\Async\Task::async(function () use ($socket) {
+		//\Phalcon\Debug::enable();
+		try {
+			$uri = $chunk = '';
+			$ret = NULL;
+			$parser = new \Phalcon\Http\Parser();
+			while($buffer = $socket->read()) {
+				$ret = $parser->execute($buffer);
+				if (!$ret) {
+					throw new \Exception('HTTP parse failed');
+				}
+				if ($parser->status() == \Phalcon\Http\Parser::STATUS_END) {
+					$uri = \Phalcon\Arr::get($ret, 'QUERY_STRING');
+					$body = \Phalcon\Arr::get($ret, 'BODY');
+					break;
+				}
+			}
+
+			if (empty($ret)) {
+				$socket->close();
+				return;
+			}
+
+			/*
+			$sendchunk = 'hello world';
+			$sendchunk = \sprintf("HTTP/1.1 200 OK\r\nServer: webserver\r\nContent-Type: text/html\r\nTransfer-Encoding: chunked\r\nConnection: close\r\n\r\n%x\r\n%s\r\n0\r\n\r\n", \strlen($sendchunk), $sendchunk);
+			$socket->write($sendchunk);
+			return;
+			*/
+
+			$app = new \Phalcon\Mvc\Micro();
+
+			// 超始路由
+			$app->get('/', function () use ($socket) {
+				
+				$sendchunk = "<h1>Welcome!</h1>";
+
+				//debug($sendchunk, __LINE__);
+				$sendchunk = \sprintf("HTTP/1.1 200 OK\r\nServer: webserver\r\nContent-Type: text/html\r\nTransfer-Encoding: chunked\r\nConnection: close\r\n\r\n%x\r\n%s\r\n0\r\n\r\n", \strlen($sendchunk), $sendchunk);
+				$socket->write($sendchunk);
+			});
+
+			$app->handle($uri);
+			return;
+
+			$di = new \Phalcon\DI;
+			$di->set('dispatcher', function () {
+				$dispatcher = new \Phalcon\Mvc\Dispatcher();
+				return $dispatcher;
+			}, TRUE);
+			$di->set('request', function () {
+				$request = new \Phalcon\Http\Request();
+				return $request;
+			}, TRUE);
+			$di->set('response', function () {
+				$response = new \Phalcon\Http\Response();
+				return $response;
+			}, TRUE);
+			$di->set('router', function () {
+				$router = new \Phalcon\Mvc\Router();
+				return $router;
+			}, TRUE);
+			$di->set('view', function () {
+				$view = new \Phalcon\Mvc\View();
+				$view->setBasePath(__DIR__.DIRECTORY_SEPARATOR.'mvc/views');
+				return $view;
+			}, TRUE);
+
+			$application = new \Phalcon\Mvc\Application;
+			$application->useImplicitView(false);
+			$sendchunk = $application->handle($uri)->getContent();
+
+			//debug($sendchunk, __LINE__);
+			$sendchunk = \sprintf("HTTP/1.1 200 OK\r\nServer: webserver\r\nContent-Type: text/html\r\nTransfer-Encoding: chunked\r\nConnection: close\r\n\r\n%x\r\n%s\r\n0\r\n\r\n", \strlen($sendchunk), $sendchunk);
+			$socket->write($sendchunk);
+		} catch (\Throwable $e) {
+			debug($e->getMessage(), __LINE__);
+		} finally {
+			//\Phalcon\Debug::disable();
+			$socket->close();
+			//debug('CLIENT DISCONNECTED', __LINE__);
+		}
+	});
+}

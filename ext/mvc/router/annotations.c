1b348cd56c2f8facc911409e77c281ef5573a193
dreamsxin <dreamsxin@126.com>
2020-11-09 15:35:25 +0800

    Updated

--- ext/mvc/router/annotations.c
+++ ext/mvc/router/annotations.c
@@ -270,6 +270,8 @@ PHP_METHOD(Phalcon_Mvc_Router_Annotations, handle){
 					 */
 					if (phalcon_array_isset_long(scope, 2)) {
 						phalcon_array_fetch_long(&module_name, scope, 2, PH_NOISY|PH_READONLY);
+					} else {
+						ZVAL_NULL(module_name);
 					}
 
 					PHALCON_CONCAT_VV(&suffixed, &handler, &controller_suffix);

e9e0327057a2a10cecb7441c1eea87eb9fb519bd
dreamsxin <dreamsxin@126.com>
2020-11-30 15:27:50 +0800

    Updated example async

--- examples/async/webserver/worker.php
+++ examples/async/webserver/worker.php
@@ -1,5 +1,58 @@
 <?php
 
+class Pool
+{
+	private $channel;
+	private $concurrency;
+	private $count = 0;
+	private $context;
+
+	public function __construct(int $concurrency = 1, int $capacity = 0)
+	{
+		$this->concurrency = max(1, $concurrency);
+		$this->channel = new \Phalcon\Async\Channel($capacity);
+		$this->context = \Phalcon\Async\Context::current();
+	}
+
+	public function close(?\Throwable $e = null): void
+	{
+		$this->count = \PHP_INT_MAX;
+		$this->channel->close($e);
+	}
+
+	public function submit(callable $work, $socket, ...$args): \Phalcon\Async\Awaitable
+	{
+		if ($this->count < $this->concurrency) {
+			$this->count++;
+			\Phalcon\Async\Task::asyncWithContext($this->context, static function (iterable $it) {
+				try {
+					foreach ($it as list ($defer, $context, $work, $socket, $args)) {
+						try {
+							$defer->resolve($context->run($work, $socket, ...$args));
+						} catch (\Throwable $e) {
+							$defer->fail($e);
+						} finally {
+						}
+					}
+				} catch (\Throwable $e) {
+				} finally {
+					--$this->count;
+				}
+			}, $this->channel->getIterator());
+		}
+
+		$this->channel->send([
+			$defer = new \Phalcon\Async\Deferred(),
+			\Phalcon\Async\Context::current(),
+			$work,
+			$socket,
+			$args
+		]);
+
+		return $defer->awaitable();
+	}
+}
+
 var_dump(Phalcon\Async\Process\Process::isWorker());
 var_dump($ipc = Phalcon\Async\Process\Process::connect());
 
@@ -16,93 +69,49 @@ function debug(string $data, $lineno = 'NULL') {
 	print_r($data);
 }
 
-while ($socket = Phalcon\Async\Network\TcpSocket::import($ipc)) {
+$worker = static function ($socket) {
+	
+	$parser = new \Phalcon\Http\Parser();
 
-	Phalcon\Async\Task::async(function () use ($socket) {
-		//\Phalcon\Debug::enable();
-		try {
-			$uri = $chunk = '';
-			$ret = NULL;
-			$parser = new \Phalcon\Http\Parser();
-			while($buffer = $socket->read()) {
+	try {
+		$buffer = '';
+		while (null !== ($chunk = $socket->read())) {
 
-				$ret = $parser->execute($buffer);
-				if (!$ret) {
-					throw new \Exception('HTTP parse failed');
-				}
-				if ($parser->status() == \Phalcon\Http\Parser::STATUS_END) {
-					$uri = \Phalcon\Arr::get($ret, 'QUERY_STRING');
-					$body = \Phalcon\Arr::get($ret, 'BODY');
-					break;
-				}
+			$ret = $parser->execute($chunk);
+			if (!$ret) {
+				throw new \Exception('HTTP parse failed');
 			}
-
-			if (empty($ret)) {
-				$socket->close();
-				return;
-			}
-
-			/*
-			$sendchunk = 'hello world';
-			$sendchunk = \sprintf("HTTP/1.1 200 OK\r\nServer: webserver\r\nContent-Type: text/html\r\nTransfer-Encoding: chunked\r\nConnection: close\r\n\r\n%x\r\n%s\r\n0\r\n\r\n", \strlen($sendchunk), $sendchunk);
-			$socket->write($sendchunk);
-			return;
-			*/
-
-			$app = new \Phalcon\Mvc\Micro();
-
-			// 超始路由
-			$app->get('/', function () use ($socket) {
+			if ($parser->status() == \Phalcon\Http\Parser::STATUS_END) {
+				$uri = \Phalcon\Arr::get($ret, 'QUERY_STRING');
+				$body = \Phalcon\Arr::get($ret, 'BODY');
 				
-				$sendchunk = "<h1>Welcome!</h1>";
-
-				//debug($sendchunk, __LINE__);
+				$sendchunk = 'hello world';
 				$sendchunk = \sprintf("HTTP/1.1 200 OK\r\nServer: webserver\r\nContent-Type: text/html\r\nTransfer-Encoding: chunked\r\nConnection: close\r\n\r\n%x\r\n%s\r\n0\r\n\r\n", \strlen($sendchunk), $sendchunk);
 				$socket->write($sendchunk);
-			});
+				/*
+				$app = new \Phalcon\Mvc\Micro();
 
-			$app->handle($uri);
-			return;
+				$app->get('/', function () use ($socket) {
+					
+					$sendchunk = "<h1>Welcome!</h1>";
+					$sendchunk = \sprintf("HTTP/1.1 200 OK\r\nServer: webserver\r\nContent-Type: text/html\r\nTransfer-Encoding: chunked\r\nConnection: close\r\n\r\n%x\r\n%s\r\n0\r\n\r\n", \strlen($sendchunk), $sendchunk);
+					$socket->write($sendchunk);
+				});
 
-			$di = new \Phalcon\DI;
-			$di->set('dispatcher', function () {
-				$dispatcher = new \Phalcon\Mvc\Dispatcher();
-				return $dispatcher;
-			}, TRUE);
-			$di->set('request', function () {
-				$request = new \Phalcon\Http\Request();
-				return $request;
-			}, TRUE);
-			$di->set('response', function () {
-				$response = new \Phalcon\Http\Response();
-				return $response;
-			}, TRUE);
-			$di->set('router', function () {
-				$router = new \Phalcon\Mvc\Router();
-				return $router;
-			}, TRUE);
-			$di->set('view', function () {
-				$view = new \Phalcon\Mvc\View();
-				$view->setBasePath(__DIR__.DIRECTORY_SEPARATOR.'mvc/views');
-				return $view;
-			}, TRUE);
-
-			$application = new \Phalcon\Mvc\Application;
-			$application->useImplicitView(false);
-			$sendchunk = $application->handle($uri)->getContent();
-
-			//debug($sendchunk, __LINE__);
-			$sendchunk = \sprintf("HTTP/1.1 200 OK\r\nServer: webserver\r\nContent-Type: text/html\r\nTransfer-Encoding: chunked\r\nConnection: close\r\n\r\n%x\r\n%s\r\n0\r\n\r\n", \strlen($sendchunk), $sendchunk);
-			$socket->write($sendchunk);
-		} catch (\Throwable $e) {
-			$sendchunk = "<h1>Not found!</h1>";
-			$sendchunk = \sprintf("HTTP/1.1 404 NOT FOUND\r\nServer: webserver\r\nContent-Type: text/html\r\nTransfer-Encoding: chunked\r\nConnection: close\r\n\r\n%x\r\n%s\r\n0\r\n\r\n", \strlen($sendchunk), $sendchunk);
-			$socket->write($sendchunk);
-			debug($e->getMessage(), __LINE__);
-		} finally {
-			//\Phalcon\Debug::disable();
-			$socket->close();
-			//debug('CLIENT DISCONNECTED', __LINE__);
+				$app->handle($uri);
+				*/
+				break;
+			}
 		}
-	});
+	} catch (\Throwable $e) {
+		self::err($e->getMessage());
+	} finally {
+		$socket->close();
+	}
+};
+
+$pool = new Pool(500, 1);
+while ($socket = Phalcon\Async\Network\TcpSocket::import($ipc)) {
+
+	$pool->submit($worker, $socket);
 }

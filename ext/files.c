70fbe1a5aceb09772c5057f98338feec316884b5
budyaya <167263214+budyaya@users.noreply.github.com>
2025-07-24 09:02:56 +0800

    fix PHP_VERSION_ID < 80200

--- ext/files.c
+++ ext/files.c
@@ -442,11 +442,15 @@ PHP_METHOD(Phalcon_Files, move){
 	RETURN_BOOL(wrapper->wops->rename(wrapper, old_name, new_name, 0, context));
 }
 
-// typedef enum {
-// 	RIT_LEAVES_ONLY = 0,
-// 	RIT_SELF_FIRST  = 1,
-// 	RIT_CHILD_FIRST = 2
-// } RecursiveIteratorMode;
+#if PHP_VERSION_ID < 80200
+
+typedef enum {
+        RIT_LEAVES_ONLY = 0,
+        RIT_SELF_FIRST  = 1,
+        RIT_CHILD_FIRST = 2
+} RecursiveIteratorMode;
+
+#endif
 
 int rmtree_iterator(zend_object_iterator *iter, void *puser)
 {
